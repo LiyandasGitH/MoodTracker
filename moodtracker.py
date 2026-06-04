6bb6c2c5d871ee5c2db5b26574d1fc41df0c09c4
liyanda <litoncpt025@student.wethinkcode.co.za>
2026-06-04 19:02:59 +0200

    feat: added log mood and format event functions

--- moodtracker.py
+++ moodtracker.py
@@ -19,7 +19,7 @@ class MoodTracker:
         for key, mood in self.mood_map.items():
             print(f"[{key}] {mood['name']}")
 
-    def get_uer_input(self):
+    def get_user_input(self):
         
         self.display_menu()
 
@@ -31,9 +31,35 @@ class MoodTracker:
         notes = input("Any specific thoughts or reasons?: ").strip()
         return choice, notes
 
-    # def log_mood(self):
+    def log_mood(self):
 
-    #     choice, notes = self.get_user_input
+        choice, notes = self.get_user_input()
 
+        if choice:
+            selected_mood = self.mood_map[choice]
+            event_data = self._format_event(selected_mood, notes)
 
+            try:
+                result = self.calendar.add_event(event_data)
+                print(f"\nSuccessfully logged: {selected_mood['name']}")
+                print(f"Link: {result.get('htmlLink')}")
 
+            except Exception as e:
+                print(f"\nFailed to log mood. Error: {e}")
+
+    def _format_event(self, mood_data, notes):
+        """
+        Private helper to format the dictionary exactly 
+        as the Google Calendar API expects.
+        """
+        today = datetime.date.today().isoformat()
+        
+        return {
+            'summary': f"Mood: {mood_data['name']}",
+            'description': notes if notes else "No notes provided.",
+            'start': {'date': today},
+            'end': {'date': today},
+            'color': mood_data['color'],
+            'transparency': 'transparent', # Sets as 'Free' so it doesn't block your schedule
+            'reminders': {'useDefault': False}
+        }
